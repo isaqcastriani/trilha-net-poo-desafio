df298376b68ae1e86b57ee5a801649c1bd7a6999
Isaque Castriani <82527480+isaqcastriani@users.noreply.github.com>
2024-09-12 21:15:50 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Teste com a classe Nokia
+        Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 3310", imei: "111111111111111", memoria: 64);
+        Console.WriteLine("Testando o Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+        Console.WriteLine();
+
+        // Teste com a classe Iphone
+        Smartphone iphone = new Iphone(numero: "7891011", modelo: "iPhone 13", imei: "222222222222222", memoria: 128);
+        Console.WriteLine("Testando o iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
